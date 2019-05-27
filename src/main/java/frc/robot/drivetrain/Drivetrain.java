ba93d4947f64379e03b1622def0c23e65eb2bbcd
Dee <35465486+20iyerd@users.noreply.github.com>
2019-05-26 20:59:31 -0400

    initial

---
+++ src/main/java/frc/robot/drivetrain/Drivetrain.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.drivetrain;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Spark;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends Subsystem {
+  Spark left_primary;
+  Spark left_secondary;
+  Spark right_primary;
+  Spark right_secondary;
+
+  public Drivetrain(){
+    left_primary = new Spark(0);
+    left_secondary = new Spark(1);
+    right_primary = new Spark(2);
+    right_secondary = new Spark(3);
+
+    
+  }
+
+  @Override
+  public void initDefaultCommand() {}
+
+  public void setLeftSpeed(double speed){
+    left_primary.set(speed);
+    left_secondary.set(speed);
+  }
+
+  public void setRightSpeed(double speed){
+    right_primary.set(speed);
+    right_secondary.set(speed);
+  }
+
+  public void setSpeed(double left, double right){
+    setLeftSpeed(left);
+    setRightSpeed(right);
+  }
+
+}
